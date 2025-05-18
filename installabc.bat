b26ebc8f4cf9362d1669a9dcdd7cae685401561e
Faircat <136176622+Breelandthecat@users.noreply.github.com>
2025-05-17 20:28:24 -0500

    normal
    normal

---
+++ installabc.bat
@@ -0,0 +1,6 @@
+curl -LO https://archive.org/download/gameactuallynormal/gameactuallynormal.zip
+mkdir TheGame
+move gameactuallynormal.zip TheGame\game.zip
+cd TheGame
+tar -xf game.zip
+"Finn's Eco recreation"
